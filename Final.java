1e59f31575ced702bb9e10d2931e5deac1831aa5
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-16 20:06:48 +0600

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,12 @@
+public class Final
+{
+	public static void main (String[] args)
+	{
+		final int a = 5;
+		System.out.println(a);
+
+		/// A error is found in this line because a is declared as a final variable.
+		///a = 3;
+		///System.out.println(a);
+	}
+}
