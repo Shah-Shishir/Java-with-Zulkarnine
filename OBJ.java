220b9555bd07be99c6a9cd5fddeea94b01555787
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-16 20:25:22 +0600

    Add files via upload

---
+++ OBJ.java
@@ -0,0 +1,13 @@
+public class OBJ
+{
+	public static void prln (Object obj)
+	{
+		System.out.println (obj);
+	}
+
+	public static void main (String[] args)
+	{
+		String str = "ABCD";
+		prln (str);
+	}
+}
